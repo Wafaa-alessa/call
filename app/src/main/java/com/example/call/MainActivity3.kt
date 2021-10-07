62243c4faea83f01e1b93933e576696c7142699e
wafaa <wafaa.alesse@hotmail.com>
2021-10-07 14:32:54 +0300

    add Bonus(activity 3 )

--- app/src/main/java/com/example/call/MainActivity3.kt
+++ app/src/main/java/com/example/call/MainActivity3.kt
@@ -1,40 +1,27 @@
 package com.example.call
 
 import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
 
-class MainActivity2 : AppCompatActivity() {
-    val toast = "Activity2"
+class MainActivity3 : AppCompatActivity() {
+    val toast = "Activity3"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-
-        val myButton = findViewById<Button>(R.id.toast_button2)
-        myButton.setOnClickListener{
-            val intent = Intent(this,MainActivity3::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_main3)
     }
 
     override fun onResume() {
         super.onResume()
-        Toast(this,"onResume Activity 2")
+        Toast(this,"onResume Activity 3")
         Log.d(toast, "onResume: ")
     }
 
-    override fun onStop() {
-        super.onStop()
-        Toast(this,"onStop Activity 2")
-        Log.d(toast, "onStop: ")
-    }
     override fun onDestroy() {
         super.onDestroy()
-        Toast(this,"onDestroy Activity 2")
+        Toast(this,"onDestroy Activity 3")
         Log.d(toast, "onDestroy: ")
     }
 
